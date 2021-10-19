fcc8a917aa036bf011af53e907ddf668a8c8cca7
aemann2 <aemann2@gmail.com>
2021-10-19 11:57:41 -0700

    updated webpack to match index.js

--- src/actions/Launches.js
+++ src/actions/Launches.js
@@ -1,27 +1,30 @@
 import LaunchService from '../services/LaunchService';
 
 export const ACTIONS = {
-  REQUEST_LAUNCHES: 'REQUEST_LAUNCHES',
-  RECEIVE_LAUNCHES: 'RECEIVE_LAUNCHES'
+	REQUEST_LAUNCHES: 'REQUEST_LAUNCHES',
+	RECEIVE_LAUNCHES: 'RECEIVE_LAUNCHES',
 };
 
 export const requestLaunches = () => ({
-  type: ACTIONS.REQUEST_LAUNCHES
+	type: ACTIONS.REQUEST_LAUNCHES,
 });
 
-const receiveLaunches = response => ({
-  type: ACTIONS.RECEIVE_LAUNCHES,
-  payload: {
-    launches: response.data
-  }
+const receiveLaunches = (response) => ({
+	type: ACTIONS.RECEIVE_LAUNCHES,
+	payload: {
+		launches: response.data,
+	},
 });
 
-export const fetchLaunches = dispatch => {
-  dispatch(requestLaunches());
-  return LaunchService.get().then(response => dispatch(receiveLaunches(response)));
+export const fetchLaunches = (dispatch) => {
+	dispatch(requestLaunches());
+	return LaunchService.get().then((response) =>
+		dispatch(receiveLaunches(response))
+	);
 };
 
-const shouldFetchLaunches = launchCollection => !launchCollection || !launchCollection.fetching;
+const shouldFetchLaunches = (launchCollection) =>
+	!launchCollection || !launchCollection.fetching;
 
 export const fetchLaunchesIfNeeded = ({ dispatch, launchCollection }) =>
-  shouldFetchLaunches(launchCollection) && fetchLaunches(dispatch);
+	shouldFetchLaunches(launchCollection) && fetchLaunches(dispatch);
